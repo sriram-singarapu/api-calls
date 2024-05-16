f1b67684d3dcda97e2d2c6df1f5fee2da633e7fe
sriram057 <sriramsingarapu2@gmail.com>
2024-05-16 16:40:19 +0530

    http request using fetch

---
+++ api-calls/src/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const fetchMovieHandler = () => {
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformData = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_data,
+          };
+        });
+        setMovies(transformData);
+      });
+  };
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
